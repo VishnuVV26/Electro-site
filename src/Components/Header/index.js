7278ae133fe1302c85c10ef1688cde431cf2a35d
VishnuVV26 <vishnusparrow26@gmail.com>
2024-03-22 14:33:20 +0530

    first commit

---
+++ src/Components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='py-4 px-4 bg-slate-300'>
+    <div>
+<h1 className='text-3xl font-semibold text-white'>Electro<span className='text-yellow-300'>City</span></h1>
+    </div>
+    </header>
+  )
+}
+
+export default Header
