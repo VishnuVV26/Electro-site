a5c4926ceea0a59bd40d7bfa874fc89932b1cb56
VishnuVV26 <vishnusparrow26@gmail.com>
2024-03-25 13:52:36 +0530

    Header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,12 +1,58 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {FiSearch,FiX} from 'react-icons/fi'
 const Header = () => {
+
+const[showSearch,setShowSearch]=useState(false);
+
+const handleSearchInput=()=>{
+  setShowSearch(!showSearch)
+}
+
   return (
-    <header className='py-4 px-4 bg-slate-300'>
-    <div>
-<h1 className='text-3xl font-semibold text-white'>Electro<span className='text-yellow-300'>City</span></h1>
-    </div>
+    // Container
+    <header className='bg-slate-300 p-5'>
+      {/* Entire Div class */}
+       <div className='flex md:gap-20 gap-9'>
+<div>
+  <h1 className='md:text-3xl text-xl text-yellow-500 font-serif font-bold'>E<span className='text-black'>lectro</span>City</h1>
+</div>
+
+{/* Desktop View */}
+<div className='py-2 relative md:block hidden'>
+  <input type='text'
+  placeholder='Search...'
+  className='w-80 px-2 rounded-sm pr-[2.5rem]'
+  />
+  <button className='p-2 inset-y-0 right-0 absolute'><FiSearch className='text-xl'/></button>
+</div>
+
+{/* Mobile view */}
+<div className='md:hidden p-1'>
+        {/* Show search icon if search input field is not visible */}
+        {!showSearch && (
+          <button onClick={handleSearchInput} className=''>
+            <FiSearch className='text-xl' />
+          </button>
+        )}
+
+        {/* Show close icon and input field if search input field is visible */}
+        {showSearch && (
+          <div className="flex">
+            <input
+              type='text'
+              placeholder='Search...'
+              className='w-44 px-2 rounded-sm pr-[2.5rem]'
+            />
+            <button onClick={handleSearchInput} className="ml-2">
+              <FiX className='text-xl' />
+            </button>
+          </div>
+        )}
+      </div>
+
+</div>
     </header>
+   
   )
 }
 
